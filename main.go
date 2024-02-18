e4f2db6307e8f8930014d386b2789eb6787b10b7
dineshkuncham <dineshkuncham77@gmail.com>
2024-02-17 18:29:03 -0800

    add chi middleware logger

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 	"time"
 
 	"github.com/dineshkuncham/rssaggregator/internal/database"
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 
@@ -49,6 +50,7 @@ func main() {
 	go startScraping(queries, 10, time.Minute)
 
 	router := chi.NewRouter()
+	router.Use(middleware.Logger)
 
 	router.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
